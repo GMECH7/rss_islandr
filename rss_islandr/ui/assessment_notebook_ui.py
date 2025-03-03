b8f06d3c4a77b15a80bf6854987d674302840b20
Giorgos <gm7.gavroleon@gmail.com>
2025-03-03 20:49:46 +0200

    Minor BUG FIX

--- rss_islandr/ui/assessment_notebook_ui.py
+++ rss_islandr/ui/assessment_notebook_ui.py
@@ -82,8 +82,7 @@ class AssessmentNoteBookUI(GeneralUITemplate):
         excel_cell = f"{parent_excel_col}{parent_excel_row + i}"
         # TODO I have altered the dynamic calculation. I must see how to do that in the future
         # excel_cell_risk = f"{parent_excel_col}{parent_excel_row + i + 1}"
-
-        excel_cell_risk = f"{parent_excel_col}17"
+        excel_cell_risk = f"{parent_excel_col}9" if parent_excel_row == 6 else f"{parent_excel_col}17"
 
         return excel_cell, excel_cell_risk
 
